14fcd63e40ccb901e735b82a3c26d369af91fda5
Sumit <sumitgk2003@gmail.com>
2024-07-27 18:38:54 +0530

    add user and video

---
+++ Project/src/models/user.model.js
@@ -0,0 +1,79 @@
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+const userSchema=mongoose.Schema({
+  username:{
+    type:String,
+    unique:true,
+    required:true,
+    lowercase:true,
+    trim:true,
+    index:true
+  },
+  email:{
+    type:String,
+    unique:true,
+    required:true,
+    lowercase:true,
+    trim:true,
+  },
+  fullName:{
+    type:String,
+    required:true,
+    trim:true,
+  },
+  password:{
+    type:String,
+    required:[true,'Password is required']
+  },
+  avatar:{
+    type:String,
+    required:true,
+  },
+  coverImage:{
+    type:String,
+  },
+  watchHistory:[
+    {
+      type:mongoose.Schema.Types.ObjectId,
+      ref:'Video'
+    }
+  ],
+},{timestamps:true});
+
+userSchema.pre('save',async function(next){
+  if(this.isModified('password')){
+    this.password=bcrypt.hash(this.password,10);
+  }
+  next();
+})
+
+userSchema.methods.isPasswordCorrect=async function(password){
+  return await bcrypt.compare(password,this.password);
+}
+
+userSchema.methods.generateAccessToken=function(){
+  return jwt.sign({
+    _id:this.id,
+    email:this.email,
+    username:this.email,
+    fullName:this.fullName
+  },
+  process.env.ACCESS_TOKEN_SECRET,
+  {
+    expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+  }
+)
+}
+
+userSchema.methods.generateRefreshTokens=function(){
+  jwt.sign({
+    _id:this.id
+  },
+  process.env.REFRESH_TOKEN_SECRET,
+  {
+    expiresIn:REFRESH_TOKEN_EXPIRY
+  }
+)
+}
+export const User=mongoose.model('User',userSchema);
